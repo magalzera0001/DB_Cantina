4403d6186ab5ff3a270f1bc8581147088ce4a3b6
magalzera0001 <diasdesouzagabriel0@gmail.com>
2025-08-29 12:02:48 -0300

    Alteração User

--- produto/models.py
+++ produto/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=100, blank=False, null=False)
-    slug = models.SlugField(unique=True, blank=False, null=False)
+    slug = models.SlugField(unique=True, null=True)
 
     def __str__(self):
         return self.nome
@@ -11,7 +11,7 @@ class Categoria(models.Model):
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100, blank=False, null=False)
-    slug = models.SlugField(unique=True, blank=False, null=False)
+    slug = models.SlugField(unique=True, null=True)
     preco = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="produtos", blank=False, null=False)
     estoque = models.IntegerField(default=0, blank=False, null=False)
--- usuario/models.py
+++ usuario/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Usuario(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     foto = models.ImageField(upload_to='user_fotos/', null=True)
 
     def __str__(self):
